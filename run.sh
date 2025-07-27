2e6b489ded8ec3b461a48ab2ea50391d7168d8ae
jonasskounielsen <jonasskou.n@gmail.com>
2025-07-27 19:53:01 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-screen -S esp8266 -X quit
-python3 ./nodemcu-uploader/nodemcu-uploader.py \
-    --port /dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0 \
-    upload init.lua
-screen -dm -S esp8266 /dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0 115200
-screen -S esp8266 -X stuff 'node.restart()\n'
-screen -r esp8266
+DEVICE_PATH='/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0'
+if [ ! -e ./nodemcu-uploader/nodemcu-uploader.py ]; then
+    git clone https://github.com/kmpm/nodemcu-uploader.git;
+fi
+if [ -c "$DEVICE_PATH" ]; then
+    if [ -r "$DEVICE_PATH" ] && [ -w "$DEVICE_PATH" ]; then
+        screen -S esp8266 -X quit
+        screen -dm -S esp8266 "$DEVICE_PATH" 115200
+        screen -S esp8266 -X stuff 'node.restart()\n'
+        screen -r esp8266
+        python3 ./nodemcu-uploader/nodemcu-uploader.py \
+            --port "$DEVICE_PATH" \
+            upload init.lua
+    else echo "$DEVICE_PATH not writable"
+    fi
+else echo "$DEVICE_PATH not found"
+fi
