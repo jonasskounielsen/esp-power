995dcdf4d208c3cc6b37da8686eeddbc55a3738b
jonasskounielsen <jonasskou.n@gmail.com>
2025-07-27 23:41:11 +0200

    Connect to wifi

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
-print("Hello, world!")
+local SSID = "";
+local PASSWORD = "";
+
+wifi.setmode(wifi.STATION);
+wifi.sta.config({
+    ssid = SSID,
+    pwd = PASSWORD,
+});
+
+print(wifi.sta.getip());
+print(wifi.sta.gethostname());
+print(wifi.sta.getrssi());
